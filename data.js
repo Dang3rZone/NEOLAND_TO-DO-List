b6c148078e92d691f181edd71198b4f57bbaa515
Andres Rosales <andresrosalespinzon@gmail.com>
2021-01-29 22:15:17 +0100

    added some gradient to background, input for taks,  priorities and the data to loop

---
+++ data.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const tasks = [
+  {
+    id: 1,
+    name: 'Practice TS & Angular',
+    prioridad: 'important',
+  },
+
+  {
+    id: 2,
+    name: 'Walk Aika',
+    prioridad: 'daily',
+  },
+
+  {
+    id: 3,
+    name: 'Do some Hacking',
+    prioridad: 'monthly',
+  },
+  {
+    id: 4,
+    name: 'Fasting',
+    prioridad: 'important',
+  },
+];
